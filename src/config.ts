c03ead6b19c34ba8c27adc85e626266645261f88
Omar <github@omaraziz.dev>
2023-11-07 23:59:24 -0800

    cleanup

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,6 @@ import { http, fallback } from 'viem'
 import { mainnet } from '@wagmi/core/chains'
 import { createConfig, createStorage } from '@wagmi/core'
 
-import { runtime } from '#/utilities.ts'
-
 export const config = createConfig({
   chains: [mainnet],
   transports: {
@@ -13,7 +11,5 @@ export const config = createConfig({
       http(`https://eth.llamarpc.com/rpc/${import.meta.env.VITE_LLAMAFOLIO_ID}`)
     ])
   },
-  storage: ['node', 'bun'].includes(runtime())
-    ? undefined
-    : createStorage({ storage: window.localStorage })
+  storage: createStorage({ storage: window.localStorage })
 })
--- src/utilities.ts
+++ src/utilities.ts
@@ -2,32 +2,6 @@ export function truncateAddress(address: string) {
   return `${address.slice(0, 6)}...${address.slice(-4)}`
 }
 
-export function humanReadableTimestamp(timestamp: string) {
-  const date = new Date(timestamp)
-  return date
-    .toLocaleDateString('en-SE', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: 'numeric',
-      minute: '2-digit',
-      second: '2-digit'
-    })
-    .replaceAll(',', '')
-}
-
 export function raise(error: unknown): never {
   throw typeof error === 'string' ? new Error(error) : error
-}
-
-export function runtime() {
-  return globalThis.navigator?.userAgent === 'Cloudflare-Workers'
-    ? 'workerd'
-    : globalThis.process?.release?.name === 'node'
-    ? 'node'
-    : !!(globalThis as any)?.Bun || !!(globalThis as any).process?.versions?.bun
-    ? 'bun'
-    : (globalThis as any)?.EdgeRuntime
-    ? 'vercel-edge'
-    : 'unknown'
 }
